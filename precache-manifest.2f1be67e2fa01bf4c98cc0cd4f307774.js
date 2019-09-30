7b2d7eca38ad62e8846537381d46ef3ba67b3d7c
Arun Biradar <arunvbiradar@gmail.com>
2019-09-30 07:07:03 +0530

    Updates

---
+++ precache-manifest.2f1be67e2fa01bf4c98cc0cd4f307774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3602a72f1a064f591a9b9da58460c5e",
+    "url": "/react-automotive/index.html"
+  },
+  {
+    "revision": "9860bb90214633e8cdfd",
+    "url": "/react-automotive/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "fb2062e2c9f9a7b2d230",
+    "url": "/react-automotive/static/css/main.f573944d.chunk.css"
+  },
+  {
+    "revision": "9860bb90214633e8cdfd",
+    "url": "/react-automotive/static/js/2.acc68719.chunk.js"
+  },
+  {
+    "revision": "fb2062e2c9f9a7b2d230",
+    "url": "/react-automotive/static/js/main.8345dc5b.chunk.js"
+  },
+  {
+    "revision": "8105fcd7eac4954c9eb5",
+    "url": "/react-automotive/static/js/runtime-main.909a7b60.js"
+  }
+]);
